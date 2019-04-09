388ea9649aa8c7d4c836817e1c6005d9c9174c95
Zac Crosby <Zacros7164@gmail.com>
2019-04-09 12:13:26 -0400

    ejected app and added css modules (old way)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 
@@ -52,7 +52,7 @@ class App extends Component {
 
   render (){
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
@@ -77,10 +77,18 @@ class App extends Component {
       )
     }
 
+    const assignedClasses = [];
+    if (this.state.persons.length <= 2){
+      assignedClasses.push(classes.red);
+    }
+    if (this.state.persons.length <= 1){
+      assignedClasses.push(classes.bold);
+    }
+
     return(
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm a React App</h1>
-          <p>This is really working!</p>
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
           <button
             style={style} 
             onClick={this.togglePersonsHandler}>Toggle Persons</button>
